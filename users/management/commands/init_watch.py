e6858b2d0e0b4408348cb8a470d9a9aa00ea284f
hajeong67 <happykhj67@gmail.com>
2025-06-02 23:05:27 +0900

    [fix] 커맨드 변경

--- users/management/commands/init_watch.py
+++ users/management/commands/init_watch.py
@@ -5,18 +5,25 @@ from users.models import Watch
 User = get_user_model()
 
 class Command(BaseCommand):
-    help = '테스트용 watch_user와 Watch 등록'
+    help = 'User와 Watch 등록'
 
     def handle(self, *args, **kwargs):
         username = 'user_1'
         password = '1111'
         device_id = 'watch-id-001'
+        email = f'{username}@example.com'
 
-        user, created = User.objects.get_or_create(username=username)
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={'email': email}
+        )
+
         if created:
             user.set_password(password)
             user.save()
             self.stdout.write(self.style.SUCCESS(f'✅ 유저 생성: {username}'))
+        else:
+            self.stdout.write(f'ℹ️ 이미 존재: {username}')
 
         watch, created = Watch.objects.get_or_create(user=user, device_id=device_id)
         if created:
