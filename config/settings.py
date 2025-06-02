a55211eb95b61a70f71ee51f7b52529fb60423bd
hajeong67 <happykhj67@gmail.com>
2025-06-02 23:27:26 +0900

    [fix]

--- config/settings.py
+++ config/settings.py
@@ -186,9 +186,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'knox.auth.TokenAuthentication',
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
