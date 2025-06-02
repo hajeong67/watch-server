5a97976299b2e1e546d0289db936204c2f118326
hajeong67 <happykhj67@gmail.com>
2025-06-02 18:02:15 +0900

    [fix] url 변경

--- watch/urls.py
+++ watch/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
     SensorDataListAPIView,
     SensorDataDetailAPIView,
+    WatchSensorDataAPIView,
 )
 
 urlpatterns = [
     path('api/sensor/', SensorDataListAPIView.as_view(), name="sensor-data-list"),       # GET
     # path('api/sensor/', AddSensorDataAPIView.as_view(), name="add-sensor-data"),         # POST
     path('api/sensor/<str:device_id>/', SensorDataDetailAPIView.as_view(), name="sensor-data-detail"),  # GET, PUT, PATCH, DELETE
+    path('api/sensor/watch/', WatchSensorDataAPIView.as_view(), name="watch-sensor"),
 ]
