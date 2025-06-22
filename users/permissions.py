994b04314d5b1a4161d05f8fb935d5b1ca0beb33
hajeong67 <happykhj67@gmail.com>
2025-06-23 00:12:11 +0900

    .

--- users/permissions.py
+++ users/permissions.py
@@ -3,4 +3,5 @@ from rest_framework.permissions import BasePermission
 class MiddlewareAuthenticated(BasePermission):
     def has_permission(self, request, view):
         print(">> [DEBUG] MiddlewareAuthenticated called")
+        print(">> [DEBUG] is_authenticated:", request.user.is_authenticated)
         return bool(request.user and request.user.is_authenticated)
