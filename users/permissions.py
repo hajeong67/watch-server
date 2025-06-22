cfbc87d63922a65276aeb2c40a0317c32d8714da
hajeong67 <happykhj67@gmail.com>
2025-06-23 00:07:11 +0900

    .

--- users/permissions.py
+++ users/permissions.py
@@ -1,8 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 class MiddlewareAuthenticated(BasePermission):
-    """
-    미들웨어로 인증된 사용자만 접근 가능
-    """
     def has_permission(self, request, view):
+        print(">> [DEBUG] MiddlewareAuthenticated called")
         return bool(request.user and request.user.is_authenticated)
